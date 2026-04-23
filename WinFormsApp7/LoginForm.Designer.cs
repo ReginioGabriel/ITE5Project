305bd0a108c12d63c975c8d03f30afe3abe29618
Rhenz <Rhenz@DESKTOP-47KUKDA>
2026-04-24 00:12:25 +0800

    Designed LoginForm

--- WinFormsApp7/LoginForm.Designer.cs
+++ WinFormsApp7/LoginForm.Designer.cs
@@ -28,28 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            btntest = new Button();
             btnSignUp = new Button();
             btnLogin = new Button();
             txtUsername = new TextBox();
             txtPassword = new TextBox();
+            label1 = new Label();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            panel2 = new Panel();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // btntest
             // 
-            button1.Location = new Point(12, 12);
-            button1.Name = "button1";
-            button1.Size = new Size(121, 23);
-            button1.TabIndex = 3;
-            button1.Text = "Test Connection";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            btntest.Location = new Point(583, 345);
+            btntest.Name = "btntest";
+            btntest.Size = new Size(87, 27);
+            btntest.TabIndex = 3;
+            btntest.Text = "Test Connection";
+            btntest.UseVisualStyleBackColor = true;
+            btntest.Click += btntest_Click;
             // 
             // btnSignUp
             // 
-            btnSignUp.Location = new Point(260, 245);
+            btnSignUp.Location = new Point(25, 164);
             btnSignUp.Name = "btnSignUp";
-            btnSignUp.Size = new Size(121, 23);
+            btnSignUp.Size = new Size(79, 26);
             btnSignUp.TabIndex = 4;
             btnSignUp.Text = "Sign Up";
             btnSignUp.UseVisualStyleBackColor = true;
@@ -57,9 +71,9 @@
             // 
             // btnLogin
             // 
-            btnLogin.Location = new Point(428, 245);
+            btnLogin.Location = new Point(137, 164);
             btnLogin.Name = "btnLogin";
-            btnLogin.Size = new Size(121, 23);
+            btnLogin.Size = new Size(79, 26);
             btnLogin.TabIndex = 5;
             btnLogin.Text = "Login";
             btnLogin.UseVisualStyleBackColor = true;
@@ -67,40 +81,162 @@
             // 
             // txtUsername
             // 
-            txtUsername.Location = new Point(294, 106);
+            txtUsername.Location = new Point(39, 65);
             txtUsername.Name = "txtUsername";
-            txtUsername.Size = new Size(206, 23);
+            txtUsername.Size = new Size(177, 21);
             txtUsername.TabIndex = 6;
+            txtUsername.Text = "Username";
+            txtUsername.TextChanged += txtUsername_TextChanged;
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(294, 166);
+            txtPassword.Location = new Point(39, 117);
             txtPassword.Name = "txtPassword";
-            txtPassword.Size = new Size(206, 23);
+            txtPassword.Size = new Size(177, 21);
             txtPassword.TabIndex = 7;
+            txtPassword.Text = "Password";
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.Highlight;
+            label1.Location = new Point(39, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(153, 25);
+            label1.TabIndex = 8;
+            label1.Text = "Sign Up or Login";
+            label1.Click += label1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BackgroundImage = Properties.Resources._44748_439982672;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label2);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(682, 42);
+            panel1.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Image = Properties.Resources.icon_logo1;
+            pictureBox1.Location = new Point(12, 9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(27, 24);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.FlatStyle = FlatStyle.Flat;
+            label2.Font = new Font("Papyrus", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(49, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 24);
+            label2.TabIndex = 10;
+            label2.Text = "Spoti Play";
+            label2.Click += label2_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Transparent;
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(pictureBox4);
+            panel2.Controls.Add(pictureBox3);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(txtUsername);
+            panel2.Controls.Add(btnSignUp);
+            panel2.Controls.Add(btnLogin);
+            panel2.Controls.Add(txtPassword);
+            panel2.Location = new Point(60, 92);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(239, 221);
+            panel2.TabIndex = 10;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.encrypted_24dp_000000_FILL0_wght400_GRAD0_opsz24;
+            pictureBox4.Location = new Point(9, 114);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(24, 24);
+            pictureBox4.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox4.TabIndex = 10;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.person_24dp_000000_FILL0_wght400_GRAD0_opsz24;
+            pictureBox3.Location = new Point(9, 62);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(24, 24);
+            pictureBox3.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox3.TabIndex = 9;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Dock = DockStyle.Fill;
+            pictureBox2.Image = Properties.Resources._44748_439982672;
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(682, 384);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 11;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
             // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(6F, 16F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(txtPassword);
-            Controls.Add(txtUsername);
-            Controls.Add(btnLogin);
-            Controls.Add(btnSignUp);
-            Controls.Add(button1);
+            ClientSize = new Size(682, 384);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(btntest);
+            Controls.Add(pictureBox2);
+            Font = new Font("Franklin Gothic Medium Cond", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "LoginForm";
+            ShowIcon = false;
             Text = "Form2";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private Button btntest;
         private Button btnSignUp;
         private Button btnLogin;
         private TextBox txtUsername;
         private TextBox txtPassword;
+        private Label label1;
+        private Panel panel1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
     }
 }
